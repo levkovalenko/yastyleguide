5e52548d23c8d5dcad64f57ca89b8c95e9715078
levkovalenko <levozavr@mail.ru>
2021-11-26 17:48:20 +0300

    add complexity checker

--- yastyleguide/checkers/__init__.py
+++ yastyleguide/checkers/__init__.py
@@ -1,3 +1,5 @@
 """Import module libs."""
 from .base import BaseChecker  # noqa: F401
+from .complexity_line import LineComplexityChecker  # noqa: F401
+from .complexity_module import ModuleComplexityChecker  # noqa: F401
 from .loop import LoopChecker  # noqa: F401
---
+++ yastyleguide/checkers/complexity_line.py
@@ -0,0 +1,37 @@
+from typing import Dict
+
+from yastyleguide.types import ERROR_GENERATOR
+from yastyleguide.visitors import ComplexityVisitor
+
+from .base import BaseChecker
+
+
+class LineComplexityChecker(BaseChecker):
+    """Check loop statement into ast tree."""
+
+    base_visitor = ComplexityVisitor
+    template = "YASG103 Line is to complex, {0} > {1}."
+    max_line_complexity: int
+
+    def _set_options(self, max_line_complexity: int = 8, **kwargs):
+        """Set checker options.
+
+        Args:
+            max_line_complexity (int, optional): max complexity. Defaults to 8.
+        """
+        self.max_line_complexity = max_line_complexity
+
+    def errors(self) -> ERROR_GENERATOR:
+        """Rule for linter errors generator.
+
+        Yield:
+            ERROR_GENERATOR: errors in flake format.
+        """
+        # We use one implementation of NodeVisitor
+        line_score: Dict[int, int] = self.visitor.line_score  # type: ignore
+        max_complexity = self.max_line_complexity
+        yield from (
+            (lineno, 0, self.template.format(score, max_complexity), type(self.visitor))
+            for lineno, score in line_score.items()
+            if score > max_complexity
+        )
